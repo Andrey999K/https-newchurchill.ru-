e1fd063aa4ae127353a07edba5268a1c88aca090
Andrey999K <andrey1234xxxx@mail.ru>
2020-06-09 00:48:46 +0300

    Update 4 phpmailer

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -1,8 +1,13 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
-   
-    const SENDER = 'info@parovozvape.com';
-    const CATCHER = 'admin@parovozvape.com';
-    const SUBJECT = 'Заявка с сайта Черчилль';
-    const CHARSET = 'UTF-8';
-    
+// require_once('phpmailer/phpmailer.php');
+
+// const SENDER = 'info@parovozvape.com';
+// const CATCHER = 'admin@parovozvape.com';
+const SENDER = '';
+const CATCHER = '';
+const SUBJECT = 'Заявка с сайта Черчилль';
+const CHARSET = 'UTF-8';
+const HOST = 'smtp.mail.ru';
+const LOGIN = '';
+const PASS = '';
+const PORT = 465;
