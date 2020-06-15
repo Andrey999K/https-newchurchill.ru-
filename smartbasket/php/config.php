ad9dc5dd60edc73cd53301297376363041af792c
Andrey999K <andrey1234xxxx@mail.ru>
2020-06-15 19:33:28 +0300

    15.06.2020

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -1,8 +1,13 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
    
-    const SENDER = 'info@parovozvape.com';
-    const CATCHER = 'admin@parovozvape.com';
+    // const SENDER = 'info@parovozvape.com';
+    // const CATCHER = 'admin@parovozvape.com';
+    const SENDER = 'andrey1234xxxx1@mail.ru';
+    const CATCHER = 'andrey1234xxxx@mail.ru';
     const SUBJECT = 'Заявка с сайта Черчилль';
     const CHARSET = 'UTF-8';
-    
+    const HOST = 'smtp.mail.ru';
+    const LOGIN = 'andrey1234xxxx1@mail.ru';
+    const PASS = '0129502Fylhtq';
+    const PORT = 465;
