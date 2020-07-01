b7ba3072e51fc9fc170a9df8143625857883b6a0
Andrey999K <andrey1234xxxx@mail.ru>
2020-07-01 18:22:19 +0300

    01.07.2020 18:22

--- form/php/config.php
+++ form/php/config.php
@@ -1,7 +1,9 @@
 <?php
-
+const SENDER = 'alextest13371@gmail.com';
+const CATCHER = 'info@parovozvape.com';
 const SUBJECT = 'Заявка с сайта Черчилль';
 const CHARSET = 'UTF-8';
 const HOST = 'smtp.gmail.com';
-
+const LOGIN = 'alextest13371@gmail.com';
+const PASS = 'Y3f-xXL-a8B-sFX';
 const PORT = 465;
